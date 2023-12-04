bfb17bc3cdd04822a4284bd7c4858d563dbaa205
cedrvn10 <brochettes40@outlook.fr>
2023-12-04 22:48:47 +0100

    New test

---
+++ BuilderHelloWorld/test/hello/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
