b5804b3336f67d1cd20f3c1df5e6d5d723fae614
cedrvn10 <brochettes40@outlook.fr>
2023-12-11 13:20:37 +0100

    Correcting le nombre le plus grand

--- Boucle/src/exercice6/exo.java
+++ Boucle/src/exercice6/exo.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class exo {
 	/*
 	 * Écrire un algorithme qui demande à l'utilisateur deux nombres et qui affiche
-	 * les nombres du plus petit au plus grand. Ex : Donnez le nbr 1 : 9 Donnez le
-	 * nbr 2 : 4 Le programme : 4 5 6 7 8 9
+	 * les nombres du plus petit au plus grand. 
+	 * Ex : Donnez le nbr 1 : 9 
+	 * Donnez le nbr 2 : 4 
+	 * Le programme : 4 5 6 7 8 9
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		int copy;
 		Scanner scanner = new Scanner(System.in);
-		
+	
 		System.out.println("Saisissez le premier nombre");
 		int nombreUn = scanner.nextInt();
 		
@@ -28,7 +30,7 @@ public class exo {
 		
 		for(int i = nombreUn + 1; i < nombreDeux; i++)
 		{
-			System.out.println(i + "");
+			System.out.print(i + " ");
 		}
 
 	}
